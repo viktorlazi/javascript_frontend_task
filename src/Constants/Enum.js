b47ebf4f9c862af1a84897c0ecd5e20f23b876bb
Viktorlazi <viktorlazi010@gmail.com>
2021-03-19 10:16:47 +0100

    enum types

---
+++ src/Constants/Enum.js
@@ -0,0 +1,21 @@
+const brands = {
+  fender:'fender',
+  epiphone:'epiphone',
+  cort:'cort'
+}
+export {brands};
+
+const types = {
+  stratocaster:'stratocaster',
+  telecaster:'telecaster',
+  singlecut:'singlecut'
+}
+export  {types};
+
+const colours = {
+  yellow:'yellow',
+  sunburn:'sunburn',
+  blue:'blue',
+  black:'black'
+}
+export {colours}
--- src/Stores/ListStore.js
+++ src/Stores/ListStore.js
@@ -1,30 +1,31 @@
 import { makeAutoObservable } from 'mobx'
+import {brands, types, colours} from '../Constants/Enum'
 
 class ListStore{
   list = []
   constructor(){
     this.list.push({
-      brand:'fender',
-      type:'stratocaster',
-      colour:'blue',
+      brand:brands.fender,
+      type:types.stratocaster,
+      colour:colours.blue,
       cost:4200      
     },
     {
-      brand:'fender',
-      type:'telecaster',
-      colour:'black',
+      brand:brands.fender,
+      type:types.telecaster,
+      colour:colours.black,
       cost:4300      
     },
     {
-      brand:'cort',
-      type:'singlecut',
-      colour:'sunburn',
-      cost:3301    
+      brand:brands.cort,
+      type:types.singlecut,
+      colour:colours.sunburn,
+      cost:3301 
     },
     {
-      brand:'epiphone',
-      type:'singlecut',
-      colour:'yellow',
+      brand:brands.epiphone,
+      type:types.singlecut,
+      colour:colours.yellow,
       cost:3300     
     }
     
