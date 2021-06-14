287006ad8cfdbe8c0ff04440dd91633f7c90874a
Viktorlazi <viktorlazi010@gmail.com>
2021-06-14 16:02:58 +0200

    server brands api

--- Server/server.js
+++ Server/server.js
@@ -22,10 +22,6 @@ const port = process.env.PORT || 3001;
 app.get('/products', (req, res)=>{
   res.json(products);
 }); 
-app.get('/brands', (req, res)=>{
-  res.json(brands);
-});
-
 app.delete('/products/:id', (req, res)=>{
   try{
     const file = fs.readFileSync('./models/products.json');
@@ -60,6 +56,42 @@ app.put('/products', (req, res)=>{
   }
 });
 
+app.get('/brands', (req, res)=>{
+  res.json(brands);
+});
+app.delete('/brands/:id', (req, res)=>{
+  try{
+    const file = fs.readFileSync('./models/brands.json');
+    let json = JSON.parse(file);
+    json = json.filter((e) => { return e.id != req.params.id });
+    fs.writeFileSync('./models/brands.json', JSON.stringify(json, null, 2));
+    res.status(204);
+  }catch(err){
+    res.status(500).send(err.json());
+  }
+});
+app.post('/brands', (req, res)=>{
+  try{
+    const file = fs.readFileSync('./models/brands.json');
+    let json = JSON.parse(file);
+    json.push(req.body);
+    fs.writeFileSync('./models/brands.json', JSON.stringify(json, null, 2));
+  }catch(err){
+    res.status(500).send(err.json());
+  }
+});
+app.put('/brands', (req, res)=>{
+  try{
+    const file = fs.readFileSync('./models/brands.json');
+    let json = JSON.parse(file);
+    const index = json.findIndex(obj => obj.id === req.body.id);
+    json[index] = req.body;
+    fs.writeFileSync('./models/brands.json', JSON.stringify(json, null, 2));
+    res.status(200);
+  }catch(err){
+    res.status(500).send(err.json());
+  }
+});
 
 app.listen(port);
 console.log('Running on port ' + port);
